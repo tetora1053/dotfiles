06920afbf81ca7fd748c1ad5a2daabf9ea353477
tetora1053 <tetora1053@gmail.com>
2023-05-07 14:56:47 +0900

    refactor

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+base_dir=$(cd $(dirname $0); pwd)
+
+mkdir -p ~/.config/alacritty
+
+# zsh
+ln -sf $base_dir/.zshrc ~/.zshrc
+# starship
+ln -sf $base_dir/starship.toml ~/.config/starship.toml
+# alacritty
+ln -sf $base_dir/alacritty.yml ~/.config/alacritty/alacritty.yml
+# tmux
+ln -sf $base_dir/tmux.conf ~/.tmux.conf
+# vim
+ln -sf $base_dir/.vimrc ~/.vimrc
