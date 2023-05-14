9aeb1ba0d050c8ef8a9bbbcb3272df628a634845
tetora1053 <tetora1053@gmail.com>
2023-05-14 16:36:33 +0900

    rename files

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ base_dir=$(cd $(dirname $0); pwd)
 mkdir -p ~/.config/alacritty
 
 # zsh
-ln -sf $base_dir/.zshrc ~/.zshrc
+ln -sf $base_dir/zshrc ~/.zshrc
 # starship
 ln -sf $base_dir/starship.toml ~/.config/starship.toml
 # alacritty
@@ -13,4 +13,4 @@ ln -sf $base_dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 # tmux
 ln -sf $base_dir/tmux.conf ~/.tmux.conf
 # vim
-ln -sf $base_dir/.vimrc ~/.vimrc
+ln -sf $base_dir/vimrc ~/.vimrc
