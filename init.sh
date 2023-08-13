7bc0ec9c20ac804c8b5d758abad73d06f36f7c0a
tetora1053 <yusuke-shinoda@c-fo.com>
2023-08-13 21:35:14 +0900

    initial git config

--- init.sh
+++ init.sh
@@ -14,3 +14,16 @@ ln -sf $base_dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf $base_dir/tmux.conf ~/.tmux.conf
 # vim
 ln -sf $base_dir/vimrc ~/.vimrc
+
+# git config
+git_config() {
+  echo "input git user name:"
+  read git_user_name
+  echo "input git user email:"
+  read git_user_email
+  git config --global user.name $git_user_name
+  git config --global user.email $git_user_email
+  git config --global core.editor vim
+  echo "git config done"
+}
+git_config
